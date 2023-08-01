7dcf849560ec31e14f3b22451aa05dd4f3d30b34
bleonit001 <bleonit001@gmail.com>
2023-08-02 01:22:05 +0200

    update

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+import './App.css';
+import { Login } from "./Login";
+import { Register } from "./Register";
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+/*test*/
+/*test*/
+/*test*/
+  return (
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
+    </div>
+  );
+}
+
+export default App;
